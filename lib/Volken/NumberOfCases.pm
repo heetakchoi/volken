603c0342a8d68b4c71263cc7ea21dd38f4cd14e4
Heetak Choi <endofhope@naver.com>
2023-05-09 17:49:21 +0900

    Permutation 추가
    경우의 수에 순열 추가

--- lib/Volken/NumberOfCases.pm
+++ lib/Volken/NumberOfCases.pm
@@ -20,27 +20,56 @@ sub nCr{
     }
     return @result_list;
 }
+sub nPr{
+    my ($self, $r_count, @neo_list) = @_;
+    my @result_list = ();
+    if(@neo_list){
+	permutation(\@result_list, $r_count, [], @neo_list);
+    }else{
+	permutation(\@result_list, $r_count, [], @{$self->{"n"}});
+    }
+    return @result_list;
+}
+sub permutation{
+    my ($ref_permutation_list, $budget_count, $ref_selected_list, @candidate_list) = @_;
+    if($budget_count < 0){
+	return;
+    }elsif($budget_count == 0){
+	push(@$ref_permutation_list, $ref_selected_list);
+	return;
+    }else{
+	my $index = 0;
+	foreach (@candidate_list){
+	    my @next_candidate_list = @candidate_list;
+	    my $selected = splice(@next_candidate_list, $index, 1);
+	    my @selected_list = @$ref_selected_list;
+	    push(@selected_list, $selected);
+	    permutation($ref_permutation_list, $budget_count -1, \@selected_list, @next_candidate_list);
+	    $index ++;
+	}
+    }
+}
 sub combination{
-    my ($ref_combination_list, $budget_count, $ref_current_list, @candidate_list) = @_;
+    my ($ref_combination_list, $budget_count, $ref_selected_list, @candidate_list) = @_;
     if($budget_count < 0){
 	return;
     }
     my $candidate_list_size = scalar(@candidate_list);
     if($budget_count == $candidate_list_size){
-	my @current_list = @$ref_current_list;
-	push(@current_list, @candidate_list);
-	push(@$ref_combination_list, \@current_list);
+	my @selected_list = @$ref_selected_list;
+	push(@selected_list, @candidate_list);
+	push(@$ref_combination_list, \@selected_list);
 	return;
     }else{
-	my $current = shift(@candidate_list);
+	my $selected = shift(@candidate_list);
 	# 선택하거나
-	my @one_current_list = @$ref_current_list;
-	push(@one_current_list, $current);
-	combination($ref_combination_list, $budget_count -1, \@one_current_list, @candidate_list);
+	my @one_selected_list = @$ref_selected_list;
+	push(@one_selected_list, $selected);
+	combination($ref_combination_list, $budget_count -1, \@one_selected_list, @candidate_list);
 	
 	# 선택하지 않거나
-	my @two_current_list = @$ref_current_list;
-	combination($ref_combination_list, $budget_count, \@two_current_list, @candidate_list);
+	my @two_selected_list = @$ref_selected_list;
+	combination($ref_combination_list, $budget_count, \@two_selected_list, @candidate_list);
 	return;
     }
 }
--- usage/nPr_nCr.pl
+++ usage/nPr_nCr.pl
@@ -7,14 +7,24 @@ use lib "../lib";
 use Volken::NumberOfCases;
 
 my $noc = Volken::NumberOfCases->new((1, 2, 3));
+
 my @result_cases = $noc->nCr(2);
 foreach my $result_case (@result_cases){
     printf "%s\n", join(" ", @$result_case);
 }
-print "===\n";
+print "---\n";
 @result_cases = $noc->nCr(2, (1, 2, 3, 4));
 foreach my $result_case (@result_cases){
     printf "%s\n", join(" ", @$result_case);
 }
-
-
+print "===\n";
+@result_cases = $noc->nPr(3);
+foreach my $result_case (@result_cases){
+    printf "%s\n", join(" ", @$result_case);
+}
+print "---\n";
+@result_cases = $noc->nPr(4, (1, 2, 3, 4));
+foreach my $result_case (@result_cases){
+    printf "%s\n", join(" ", @$result_case);
+}
+print "===\n";
