7b6a207fef82d9065013ed2673ee596062165221
endofhope@naver.com <eoh@AL01772221>
2023-08-27 17:50:07 +0900

    multipart 파라메터 테스트 사이트로 위키백과 지정

--- usage/multipart/send.pl
+++ usage/multipart/send.pl
@@ -8,19 +8,19 @@ use Volken::Https;
 use Volken::Part;
 
 my $https = Volken::Https->new;
-$https->host("HOST")->url("URL");
+$https->host("ko.wikipedia.org")->url("/w/index.php");
 
 my $part1 = Volken::Part->new;
 $part1->name("upload")->filename("drug.jpg")->filelocation("drug.jpg");
 $https->add_part($part1);
 
-# my $part2 = Volken::Part->new;
-# $part2->name("somebody")->value("someone");
-# $https->add_part($part2);
+my $part2 = Volken::Part->new;
+$part2->name("title")->value("dummy");
+$https->add_part($part2);
 
-# my $part3 = Volken::Part->new;
-# $part3->name("submit")->value("upload");
-# $https->add_part($part3);
+my $part3 = Volken::Part->new;
+$part3->name("action")->value("info");
+$https->add_part($part3);
 
 # $https->emulate_flag(1);
 print $https->multipart;
